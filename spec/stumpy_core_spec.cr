3a38597ed4d09b1d9c8f2887965078c85fafd1fb
Leon Rische <leon.rische@me.com>
2017-06-26 23:01:31 +0200

    Add some tests

---
+++ spec/stumpy_core_spec.cr
@@ -0,0 +1,62 @@
+require "./spec_helper"
+
+include StumpyCore
+
+describe StumpyCore do
+  describe RGBA do
+    describe ".from_relative" do
+      it "is reversible" do
+        tuple = {0.2, 0.4, 0.6, 0.8}
+        color = RGBA.from_relative(tuple)
+        color.to_relative.should eq(tuple)
+      end
+    end
+
+    describe ".from_hex" do
+      it "handles shorthands" do
+        a = RGBA.from_hex("#123")
+        b = RGBA.from_hex("#112233")
+
+        a.should eq(b)
+      end
+
+      it "handles colors w/ alpha" do
+        a = RGBA.from_hex("#F123")
+        b = RGBA.from_hex("#FF112233")
+
+        a.should eq(b)
+        b.should eq(RGBA.from_hex("#112233"))
+      end
+    end
+
+    describe ".from_gray_n" do
+      it "handles 1-bit values" do
+        RGBA.from_gray_n(0, 1).should eq(RGBA.from_hex("#000"))
+        RGBA.from_gray_n(1, 1).should eq(RGBA.from_hex("#fff"))
+      end
+
+      it "handles 8-bit values" do
+        RGBA.from_gray_n(0, 8).should eq(RGBA.from_hex("#000"))
+        RGBA.from_gray_n(255, 8).should eq(RGBA.from_hex("#fff"))
+
+        v = 180
+        color = RGBA.from_gray_n(v, 8)
+        color.r.should eq(UInt16::MAX / 255 * v)
+      end
+    end
+
+    describe ".from_rgb_n" do
+      it "handles 1-bit values" do
+        RGBA.from_rgb_n({0, 0, 0}, 1).should eq(RGBA.from_hex("#000"))
+        RGBA.from_rgb_n({1, 0, 0}, 1).should eq(RGBA.from_hex("#f00"))
+        RGBA.from_rgb_n({1, 1, 1}, 1).should eq(RGBA.from_hex("#fff"))
+      end
+
+      it "handles 8-bit values" do
+        c = RGBA.from_hex("#abcdef")
+        vs = c.to_rgb8
+        RGBA.from_rgb_n(vs, 8).should eq(c)
+      end
+    end
+  end
+end
